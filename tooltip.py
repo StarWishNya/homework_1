cebbdf818d3186f5b57c4fa7908ce244b74fd8af
StarWishNya <StarWishNya@outlook.com>
2023-12-07 12:43:14 +0800

    修改了缩进

--- tooltip.py
+++ tooltip.py
@@ -1,27 +1,27 @@
 import tkinter as tk
 
 class ToolTip(object):#鼠标悬停提示框
-    def __init__(self, widget, text):
-        self.widget = widget
-        self.text = text
-        self.tip_window = None
-        self.id = None
-        self.x = self.y = 0
-        self._id = self.widget.bind("<Enter>", self.show_tip)
-        self.widget.bind("<Leave>", self.hide_tip)
+    def __init__(self,widget,text):
+        self.widget=widget
+        self.text=text
+        self.tip_window=None
+        self.id=None
+        self.x=self.y=0
+        self._id=self.widget.bind("<Enter>",self.show_tip)
+        self.widget.bind("<Leave>",self.hide_tip)
 
-    def show_tip(self, event=None):#显示提示框
-        x, y, _, _ = self.widget.bbox("insert")
-        x += self.widget.winfo_rootx() + 25
-        y += self.widget.winfo_rooty() + 20
-        self.tip_window = tw = tk.Toplevel(self.widget)
+    def show_tip(self,event=None):#显示提示框
+        x,y,_,_=self.widget.bbox("insert")
+        x+=self.widget.winfo_rootx()+25
+        y+=self.widget.winfo_rooty()+20
+        self.tip_window=tw=tk.Toplevel(self.widget)
         tw.wm_overrideredirect(True)
         tw.wm_geometry(f"+{x}+{y}")
-        label = tk.Label(tw, text=self.text, background="#ffffe0", relief="solid", borderwidth=1)
+        label=tk.Label(tw, text=self.text, background="#ffffe0", relief="solid", borderwidth=1)
         label.pack(ipadx=1)
 
-    def hide_tip(self, event=None):#隐藏提示框
-        tw = self.tip_window
-        self.tip_window = None
+    def hide_tip(self,event=None):#隐藏提示框
+        tw=self.tip_window
+        self.tip_window=None
         if tw:
             tw.destroy()
