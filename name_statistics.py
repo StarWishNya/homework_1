fd905f185374df920c593792f54c4512a77a84a2
StarWishNya <StarWishNya@outlook.com>
2023-12-07 12:16:49 +0800

    删除测试用例

--- name_statistics.py
+++ name_statistics.py
@@ -24,7 +24,3 @@ def Namestatistics(namelist,file_path="data"):
         name_counts_total[name]=singlenamestatistics(name,file_path)
     name_counts_total=sorted(name_counts_total.items(),key=lambda x:x[1][fullname(x[0])],reverse=True)
     return name_counts_total
-def testing():
-    Namelist=("泽村 英梨梨","安艺 伦也","加藤 惠")
-    print(Namestatistics(Namelist))
-#testing()
